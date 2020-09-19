324bc3e33309e4732d570e77ba985c7411e14636
Robson Lima <rsl.robson@gmail.com>
2020-09-19 17:47:15 -0300

    Commit inicial do projeto

---
+++ src/Aluno.java
@@ -0,0 +1,29 @@
+import java.util.Objects;
+
+public class Aluno {
+    private String nome;
+
+    public Aluno(String nome) {
+        this.nome = nome;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        Aluno aluno = (Aluno) obj;
+        return aluno.getNome().equals(this.nome);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(nome);
+    }
+
+    @Override
+    public String toString() {
+        return nome;
+    }
+}
