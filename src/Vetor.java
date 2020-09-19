1aad4ca95d3b9a24bdfcf07bde0c4c9f3460412e
Robson Lima <rsl.robson@gmail.com>
2020-09-19 18:01:02 -0300

    Implementando o método adiciona no vetor de forma O(1)

--- src/Vetor.java
+++ src/Vetor.java
@@ -1,16 +1,22 @@
 import java.util.Arrays;
 
 public class Vetor {
+
     private Aluno[] alunos = new Aluno[100];
+    private int totalDeAlunos = 0;
 
     public void adiciona(Aluno aluno) {
-        for (int i = 0; i < alunos.length; i++) {
-            if (alunos[i] == null) {
-                alunos[i] = aluno;
-                break;
-            }
-        }
+        //Complexidade O(1) para tempo, muito melhor
+        this.alunos[totalDeAlunos] = aluno;
+        totalDeAlunos++;
 
+        //Complexidade O(n) para tempo
+//        for (int i = 0; i < alunos.length; i++) {
+//            if (alunos[i] == null) {
+//                alunos[i] = aluno;
+//                break;
+//            }
+//        }
     }
 
     public void remove(Aluno aluno) {
