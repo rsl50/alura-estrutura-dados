1bb386efabe4a463b2884b248785af859412958a
Robson Lima <rsl.robson@gmail.com>
2020-09-29 21:50:36 -0300

    Implementando um conjunto

---
+++ src/conjunto/Conjunto.java
@@ -0,0 +1,33 @@
+package conjunto;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class Conjunto {
+
+    private LinkedList<LinkedList<String>> tabela = new LinkedList<LinkedList<String>>();
+
+    public Conjunto() {
+        for (int i = 0; i < 26; i++) {
+            tabela.add(new LinkedList<String>());
+        }
+    }
+
+    public void adiciona(String palavra) {
+        if (!contem(palavra)) {
+            int indice = calculaIndiceDaTabela(palavra);
+            List<String> lista = tabela.get(indice);
+            lista.add(palavra);
+        }
+    }
+
+    private boolean contem(String palavra) {
+        int indice = calculaIndiceDaTabela(palavra);
+        return tabela.get(indice).contains(palavra);
+    }
+
+    private int calculaIndiceDaTabela(String palavra) {
+        return palavra.toLowerCase().charAt(0) % 26;
+    }
+
+}
---
+++ src/conjunto/TesteDoConjunto.java
@@ -0,0 +1,8 @@
+package conjunto;
+
+public class TesteDoConjunto {
+
+    public static void main(String[] args) {
+        
+    }
+}
